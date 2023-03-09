a80de4abeaeadbf106f1cad58bab94bae923c5f9
JihyeokKim <87352996+hyeokzzi@users.noreply.github.com>
2023-03-09 13:46:08 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<queue>
+
+int find_long_arr(int *arr, int index, int size);
+int result_arr[1000];
+int main() {
+	
+	int A_size;
+	std::cin >> A_size;
+	int A_arr[1000] = { 0, };
+	for (int i = 0; i < A_size; i++) {
+		std::cin >> A_arr[i];
+	}
+	int max = 0;
+	for (int i = 0; i < A_size; i++) {
+		int t = find_long_arr(A_arr, i, A_size);
+		if (max < t) {
+			max = t;
+		}
+	}
+	std::cout << max;
+
+	return 0;
+}
+
+int find_long_arr(int* arr, int index, int size) {
+	if (result_arr[index] == 0) {
+		for (int i = index + 1; i < size; i++) {
+			if (arr[index] < arr[i]) {
+				int t = find_long_arr(arr, i, size);
+				if (t + 1 > result_arr[index]) {
+					result_arr[index] = t + 1;
+				}
+			}
+		}
+		if (result_arr[index] == 0) {
+			result_arr[index] = 1;
+		}
+	}
+	return result_arr[index];
+}
