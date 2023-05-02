bcd2b4aa7179ce6b27000dad17cc55cc5eb77ac6
JihyeokKim <87352996+hyeokzzi@users.noreply.github.com>
2023-05-02 17:16:11 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,24 +1,14 @@
 #include<iostream>
-int min_count = 99999999;
-void dp(int N, int count) {
-	if (count > min_count) return;
-	if (N == 1) {
-		if (min_count > count) {
-			min_count = count;
-		}
-		return;
-	}
-	if (N % 3 == 0) dp(N / 3, count + 1);
-	if (N % 2 == 0) dp(N / 2, count + 1);
-	dp(N - 1, count + 1);
-	return;
-}
-
+int dp[1000000] = { 0, };
 int main() {
 	int N;
 	std::cin >> N;
-	dp(N, 0);
-	std::cout << min_count;
+	for (int i = 2; i <= N; i++) {
+		dp[i] = dp[i - 1] + 1;
+		if (i % 3 == 0) dp[i] = std::min(dp[i], dp[i / 3] + 1);
+		if (i % 2 == 0) dp[i] = std::min(dp[i], dp[i / 2] + 1);
+	}
+	std::cout << dp[N];
 
 	return 0;
 }
