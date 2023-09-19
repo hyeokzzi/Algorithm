625e8b32b3aeae9791e3ae62465a3bac2ecdd191
JihyeokKim <87352996+hyeokzzi@users.noreply.github.com>
2023-09-19 22:59:51 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 128 ms, Memory: 6124 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,41 +1,31 @@
 #include<iostream>
-
-typedef struct node {
-	int num;
-	int stack;
-}node;
-node arr[1025][1025];
-int mul_sum(int x1, int y1, int x2, int y2);
-
+using namespace std;
+int arr[1025][1025];
 int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    int N, M;
+    cin >> N >> M;
+    
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> arr[i][j];
+            arr[i][j] += arr[i][j - 1];
+        }
+    }
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            arr[i][j] += arr[i - 1][j];
+        }
+    }
+    for (int i = 0; i < M; i++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
 
-	std::cin.tie(0);
-	std::cout.tie(0);
-	std::ios::sync_with_stdio(0);
+        int ans = arr[x2][y2] - (arr[x2][y1 - 1] + arr[x1 - 1][y2]) + arr[x1 - 1][y1 - 1];
+        cout << ans << '\n';
 
-	int N, M;
-	std::cin >> N >> M;
-	for (int i = 1; i <= N; i++) {
-		int sum = 0;
-		for (int j = 1; j <= N; j++) {
-			std::cin >> arr[i][j].num;
-			sum += arr[i][j].num;
-			arr[i][j].stack = sum;
-		}
-	}
-
-	for (int i = 0; i < M; i++) {
-		int x1, y1, x2, y2;
-		std::cin >> x1 >> y1 >> x2 >> y2;
-		std::cout << mul_sum(x1, y1, x2, y2) << '\n';
-	}
-	return 0;
-}
-
-int mul_sum(int x1, int y1, int x2, int y2) {
-	int sum = 0;
-	for (int i = x1; i <= x2; i++) {
-		sum += arr[i][y2].stack - arr[i][y1-1].stack;
-	}
-	return sum;
+    }
+    return 0;
 }
