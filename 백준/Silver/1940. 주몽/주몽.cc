dd48c601d83c13fd6bb9afaeff51c2ae8599ff0c
JihyeokKim <87352996+hyeokzzi@users.noreply.github.com>
2023-09-20 12:20:30 +0900

    [Silver IV] Title: 주몽, Time: 8 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -15,10 +15,19 @@ void input() {
 void solution() {
 	sort(arr.begin(), arr.end());
 	int cnt = 0;
-	for (int i = 0; i < N - 1; i++) {
-		for (int j = i + 1; j < N; j++) {
-			if (arr[i] + arr[j] == M) cnt++;
-			else if (arr[i] + arr[j] > M) break;
+	int start_idx = 0;
+	int end_idx = N - 1;
+	while (start_idx < end_idx) {
+		if (arr[start_idx] + arr[end_idx] > M) {
+			end_idx--;
+		}
+		else if (arr[start_idx] + arr[end_idx] < M) {
+			start_idx++;
+		}
+		else {
+			start_idx++;
+			end_idx--;
+			cnt++;
 		}
 	}
 	cout << cnt;
