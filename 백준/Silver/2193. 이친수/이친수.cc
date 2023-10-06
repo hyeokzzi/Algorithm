d89cafc7f319fb04d1d5eba2212785efa7415c22
JihyeokKim <87352996+hyeokzzi@users.noreply.github.com>
2023-10-06 11:35:41 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+long long dp[91];
+int main() {
+	int N;
+	cin >> N;
+	dp[1] = 1;
+	dp[2] = 1;
+	for (int i = 3; i <= N; i++ ) {
+		dp[i] = dp[i - 2] + dp[i - 1];
+	}
+	cout << dp[N];
+
+	return 0;
+}
