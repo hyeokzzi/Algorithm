8a05682579a3ad5151c884d2c9248ec67c8dde43
JihyeokKim <87352996+hyeokzzi@users.noreply.github.com>
2023-11-15 20:34:39 +0900

    [Gold III] Title: 소수의 연속합, Time: 76 ms, Memory: 20728 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -1,45 +1,37 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int N;
-vector<int> arr;
+int N, arr[4000001];
+vector<int> vec;
+int main(int argc, char** argv)
+{
+  cin >> N;
+  for(int i = 2; i<= N; i++){
+    arr[i] = i;
+  }
+  for(int i = 2; i <= N; i++){
+    if(arr[i] == i){
+      vec.push_back(i);
+      for(int j = i*2; j<= N; j += i) arr[j] = 0;
+    }
+  }
 
-bool find(int n) {
-	for (int i = 2; i * i <= n; i++) {
-		if (n % i == 0) return false;
-	}
-	return true;
-}
-
-void input() {
-	cin >> N;
-	for (int i = 2; i <= N; i++) {
-		if (find(i)) arr.push_back(i);
-	}
-}
-
-void solution() {
-	
-	int size = arr.size();
-	int end = 0;
-	int cnt = 0;
-	int sum = 0;
-	for (int i = 0; i < size; i++) {
-		while (end < size && sum < N) {
-			sum += arr[end];
-			end++;
-		}
-		if (sum == N) cnt++;
-		sum -= arr[i];
-	}
-	cout << cnt;
-
-}
-
-int main() {
-
-	input();
-	solution();
-
-	return 0;
+  int st = 0, en = 1, cnt = 0;
+  if(vec.empty()) {
+    cout << "0";
+    return 0;
+  }
+  int sum = vec[st];
+  int size = vec.size();
+  while(st < size){
+    while(en < size && sum + vec[en] <= N){
+      sum += vec[en];
+      en++;
+    }
+    if(sum == N) cnt++;
+    sum -= vec[st];
+    st++;
+  }
+  cout << cnt;
+   return 0;
 }
