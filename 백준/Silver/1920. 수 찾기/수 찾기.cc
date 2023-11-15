1de73739fcfec90cb09633efcf3b93c948807a9f
JihyeokKim <87352996+hyeokzzi@users.noreply.github.com>
2023-11-16 00:02:17 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,36 +1,45 @@
 #include<iostream>
-#include<set>
+#include<algorithm>
 using namespace std;
-int N, M;
-set<int> arr;
-void input() {
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		int num;
-		cin >> num;
-		arr.insert(num);
-	}
+int arr[100001], N, M;
+
+void input(){
+  cin >> N;
+  for(int i = 0; i < N; i++) cin >> arr[i];
+}
+bool func(int t){
+  int st = 0, en = N - 1;
+  int mid = (st + en) / 2;
+  bool checked = false;
+  while(st < en){
+    if(arr[mid] < t){
+      st = mid + 1;
+    }
+    else if(arr[mid] >= t){
+      en = mid;
+    }
+    mid = (st + en) / 2;
+  }
+  if(arr[en] == t) return true;
+  else return false;
 }
 
-void solution() {
-	cin >> M;
-	for (int i = 0; i < M; i++) {
-		int num;
-		cin >> num;
-		if (arr.find(num) == arr.end()) {
-			cout << '0' << '\n';
-		}
-		else {
-			cout << '1' << '\n';
-		}
-	}
+void solution(){
+  sort(arr, arr+N);
+  cin >> M;
+  for(int i = 0; i < M; i++){
+    int value;
+    cin >> value;
+    cout << func(value) << '\n';
+  }
 }
 
-int main() {
-	cin.tie(0);
-	cout.tie(0);
-	ios_base::sync_with_stdio(0);
-	input();
-	solution();
-	return 0;
+int main(int argc, char** argv)
+{
+  cin.tie(0);
+  cout.tie(0);
+  ios_base::sync_with_stdio(0);
+  input();
+  solution();
+   return 0;
 }
