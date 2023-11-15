c20998c8fa0e9f93be8332dd0d4bdbbaa000939f
JihyeokKim <87352996+hyeokzzi@users.noreply.github.com>
2023-11-15 20:12:53 +0900

    [Gold IV] Title: 부분합, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -1,38 +1,37 @@
 #include<iostream>
-#include<algorithm>
 using namespace std;
-int N, M;
+int N, S;
 int arr[100001];
-int min_dist = 100001;
 
-void input() {
-	cin >> N >> M;
-	for (int i = 0; i < N; i++) {
-		cin >> arr[i];
-	}
+void input(){
+  cin >> N >> S;
+  for(int i =0; i < N; i++){
+    cin >> arr[i];
+  }
 }
 
-void solution() {
-	int end = 0;
-	int sum = 0;
-	for (int i = 0; i < N; i++) {
-		while (end < N && sum < M) {
-			sum += arr[end];
-			end++;
-		}
-		if (end == N && sum < M) break;
-		min_dist = min(min_dist, end - i);
-		sum -= arr[i];
-	}
-	if (min_dist == 100001) cout << "0";
-	else cout << min_dist;
-	return;
+void solution(){
+  int st = 0;
+  int en = 1;
+  int sum = arr[st];
+  int len = 1000000;
+  while(st < N){
+    while(en < N && sum < S){
+      sum+= arr[en];
+      en++;
+    }
+    if( sum >= S ) len = min(len, en - st);
+    sum -= arr[st];
+    st++;
+  }
+  if(len > 100000) cout << "0";
+  else cout << len;
 }
 
-int main() {
+int main(int argc, char** argv)
+{
+  input();
+  solution();
 
-	input();
-	solution();
-
-	return 0;
+   return 0;
 }
