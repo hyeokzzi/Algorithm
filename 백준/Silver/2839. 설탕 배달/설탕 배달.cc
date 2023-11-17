da513d581f05b0721d92f37768e874164f6a0268
JihyeokKim <87352996+hyeokzzi@users.noreply.github.com>
2023-11-17 10:16:04 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -1,46 +1,23 @@
 #include<iostream>
 using namespace std;
-int main()
+int N;
+
+int main(int argc, char** argv)
 {
-	int height;
-	cin >> height;
-	int save_height = height;
-	int cnt_five = 0;
-	int cnt_three = 0;
-	
-	int save_five_cnt = 0;
-	int three_flag = 0;
-
-	while (height != 0)
-	{
-		if (height % 3 == 0)
-		{
-			three_flag++;
-			save_height = height;
-			save_five_cnt = cnt_five;
-		}
-
-		if (height >= 5)
-		{
-			cnt_five++;
-			height -= 5;
-		}
-		else
-		{
-			if (height - 3 < 0)
-			{
-				if (three_flag > 0)
-				{
-					cout << save_height / 3 + save_five_cnt;
-					return 0;
-				}
-				cout << "-1";
-				return 0;
-			}
-			cnt_three++;
-			height -= 3;
-		}
-	}
-	cout << cnt_five + cnt_three;
-	return 0;
+  cin >> N;
+  int M = N / 5;
+  int K = N % 5;
+  while(M > 0){
+    if(K % 3 == 0) break;
+    K += 5;
+    M--;
+  }
+  if(K % 3 != 0){
+    cout << "-1";
+  }
+  else{
+    cout << M + K / 3;
+  }
+  
+   return 0;
 }
